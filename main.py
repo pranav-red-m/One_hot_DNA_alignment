e1229f259f4995270d64d70046801fbe6ee9434b
zia <zia.kadijah007@gmail.com>
2026-03-25 21:37:59 +0530

    fix: alignment function working with the test cases

--- main.py
+++ main.py
@@ -2,13 +2,29 @@ from similar import cosine_similarity, svd_fingerprint, svd_distance
 
 from alignment import align, format_alignment
 
-# dummy input to test the alignment function 
-seq1 = "ACGTAC"
-seq2 = "CGT"
+test_cases = [
+    ("ACGTACGT", "ACGTACGT"),   # identical
+    ("ACGTACGT", "TTTTTTTT"),   # very different
+    ("ACGTACGT", "TGCAACGT"),   # similar composition
+    ("ACGTACGTACGT", "ACGT"),       # mostly similar
+    ("ACGT", "TGCA")            # reversed order
+]
 
-best_score, best_shift, scores = align(seq1, seq2)
+for seq1, seq2 in test_cases:
+    print("===================================")
+    print(f"Sequence 1: {seq1}")
+    print(f"Sequence 2: {seq2}")
 
-a1, a2 = format_alignment(seq1, seq2, best_shift)
+    best_score, best_shift, scores = align(seq1, seq2)
+    a1, a2 = format_alignment(seq1, seq2, best_shift)
+
+    print("\nBest Alignment:\n")
+    print(a1)
+    print(a2)
+
+    print("\nBest Score:", best_score)
+    print("Best Shift:", best_shift)
+    print()
 
 print("Best Alignment:\n")
 print(a1)
